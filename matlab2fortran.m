0c788006a9abdd79a9cfbc4c2a04de90d75b182f
elmanuelito <trashy.trashos@gmail.com>
2013-10-16 14:44:18 +0200

    New readme

--- matlab2fortran.m
+++ matlab2fortran.m
@@ -15,14 +15,14 @@
 % License: None. Thank you for sharing your improvements to me by email.
 % 
 % 
-% # DESCRIPTION
+% ## DESCRIPTION
 % 
 % matlab2fortran(matlab-to-fortran) is a code that performs an automatic conversion of one or multiple matlab files to fortran format. The script translate some of the main features of matlab to fortran. matlab2fortran performs a quick and dirty conversion on a line-by-line basis (but still supports conditional loops, subroutine). The script allows multiple matlab command per line if these commands are separated by ";", but not by ",".
 % 
 % matlab2fortran does not intend to produce ready-to-compile code, but performs several necessary conversions. The generated code keeps the structure, variables names and comments of the original code. 
 % 
 % 
-% # INSTALLATION AND REQUIREMENTS
+% ## INSTALLATION AND REQUIREMENTS
 % 
 % The script is located in the release directory of the repository.
 % The script matlab2fortran (matlab-to-fortran) is a single script file written in matlab. It does not require a particular installation other than maybe adding this script to your current folder or matlab path. 
@@ -34,7 +34,7 @@
 % 
 % 
 % 
-% # REVISIONS
+% ## REVISIONS
 % 
 % 14/12/12 : first release
 % 
@@ -53,9 +53,9 @@
 % 
 % 
 % 
-% # FEATURES AND TODOs
+% ## FEATURES AND TODOs
 % 
-% ## Main Features of matlab2fortran:
+% ### Main Features of matlab2fortran:
 % 
 % - conversion of nested structure: do, if, switch (select), while
 % - conversion of function to subroutine with recognition of output and input arguments, (output arguments are put at the end)
@@ -75,7 +75,7 @@
 % - replaces end by size(XXX,XXX)
 % - Provides Doxygen compatible comments for subroutine and arguments
 % 
-% ## TODOs:
+% ### TODOs:
 % 
 % - easy: replace also & and | after && and ||
 % - better printf handling
@@ -84,7 +84,7 @@
 % - isequal
 % 
 % 
-% ## Features that are not intented to be supported
+% ### Features that are not intented to be supported
 % 
 % - Ready-to-compile output and perfection...
 % - Parsing of line and tree-like syntax detection
@@ -93,13 +93,13 @@
 % - Detecting all variables
 % 
 % 
-% # RUN INSTRUCTIONS and PARAMETERS
+% ## RUN INSTRUCTIONS and PARAMETERS
 % 
 % The script should run in few seconds for a file a 1000 lines (say 2s). If it takes longer, activate the Debug flag bDebug=1; to see where the script is stuck...
 % Several parameters are found at the beginning of the script.
 % 
 % 
-% # EXAMPLES
+% ## EXAMPLES
 % 
 % example 1: one file
 % matlab2fortran('tests/test1.m');
