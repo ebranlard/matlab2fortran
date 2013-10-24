7ecb58fdb28facff16d1bf047b8d294f292dfa5d
elmanuelito <elmanuelito.github@gmail.com>
2013-10-24 14:10:00 +0200

    Bug fixed for expressions with bracket but no equal (i.e. not a function)

--- dev/freplacefunctioncall.m
+++ dev/freplacefunctioncall.m
@@ -9,46 +9,48 @@ ieq=strfind(s,'=');
 iob=strfind(s,'[');
 icb=strfind(s,']');
 
-[b iop icp]=ffindmatching(s,ieq(1),'(',')',1);
-% output arguments
-isf=ieq+1;
-s_argout=strtrim(s((iob+1):(icb-1)));
-s_argout=strrep(s_argout,', ',',');
-s_argout=strrep(s_argout,' ,',',');
-s_argout=fremovespaces(s_argout);
-s_argout=strrep(s_argout,' ',',');
-% [~, ~, ~, ~, ~, ~, c_argin]=regexp(s_argin,',');
-[~, ~, ~, ~, ~, ~, c_argout]=regexp(s_argout,',');
+if ~isempty(ieq)
+    [~,iop,icp]=ffindmatching(s,ieq(1),'(',')',1);
+    % output arguments
+    isf=ieq+1;
+    s_argout=strtrim(s((iob+1):(icb-1)));
+    s_argout=strrep(s_argout,', ',',');
+    s_argout=strrep(s_argout,' ,',',');
+    s_argout=fremovespaces(s_argout);
+    s_argout=strrep(s_argout,' ',',');
+    % [~, ~, ~, ~, ~, ~, c_argin]=regexp(s_argin,',');
+    [~, ~, ~, ~, ~, ~, c_argout]=regexp(s_argout,',');
 
-% function name
-fname=strtrim(s(isf:(iop-1)));
-% arguments
-s_argin=s((iop+1):(icp-1));
+    % function name
+    fname=strtrim(s(isf:(iop-1)));
+    % arguments
+    s_argin=s((iop+1):(icp-1));
 
-% concatenation, don't care about inout for now...
-if ~isempty(s_argout) && ~isempty(s_argin)
-    s_args = [s_argin ' , ' s_argout];
-elseif isempty(s_argout) && ~isempty(s_argin)
-    s_args= s_argin;
-elseif isempty(s_argout) && isempty(s_argin)
-    s_args= s_argout;
-else
-    s_args='';
+    % concatenation, don't care about inout for now...
+    if ~isempty(s_argout) && ~isempty(s_argin)
+        s_args = [s_argin ' , ' s_argout];
+    elseif isempty(s_argout) && ~isempty(s_argin)
+        s_args= s_argin;
+    elseif isempty(s_argout) && isempty(s_argin)
+        s_args= s_argout;
+    else
+        s_args='';
+    end
+
+    scomm=s((icp+1):end);
+
+    % To tring
+    sf=['call ' fname '( ' s_args  ') ' scomm '\n'];
+
+    % Declarations
+    for iout = 1:length(c_argout)
+        v.name=strtrim(c_argout{iout});
+        [v.type, v.shape, v.prop] =fgetVarTypeFromName(v.name);
+        v.comment='';
+        decl_stack=fstack_push(decl_stack,v);
+    end
+
 end
-
-scomm=s((icp+1):end);
-
-% To tring
-sf=['call ' fname '( ' s_args  ') ' scomm '\n'];
-
-% Declarations
-for iout = 1:length(c_argout)
-    v.name=strtrim(c_argout{iout});
-    [v.type, v.shape, v.prop] =fgetVarTypeFromName(v.name);
-    v.comment='';
-    decl_stack=fstack_push(decl_stack,v);
-end
-
 
 end %function
 
--- matlab2fortran.m
+++ matlab2fortran.m
@@ -6,8 +6,8 @@
 % * Usage: matlab2fortran(filename);
 % * Author:Emmanuel Branlard (contributors are welcome)
 % * Creation Date  : December 2012
-% * Last revision  : 2013-10-16
-% * Version: 1.0-18-g1c9c30b 
+% * Last revision  : 2013-10-17
+% * Version: 1.0-19-gba00dae 
 % * Web-Sites: 
 %     - http://github.com/elmanuelito/matlab2fortran
 %     - http://emmanuel.branlard.free.fr/work/programming/
@@ -1100,46 +1100,48 @@ ieq=strfind(s,'=');
 iob=strfind(s,'[');
 icb=strfind(s,']');
 
-[b iop icp]=ffindmatching(s,ieq(1),'(',')',1);
-% output arguments
-isf=ieq+1;
-s_argout=strtrim(s((iob+1):(icb-1)));
-s_argout=strrep(s_argout,', ',',');
-s_argout=strrep(s_argout,' ,',',');
-s_argout=fremovespaces(s_argout);
-s_argout=strrep(s_argout,' ',',');
-% [~, ~, ~, ~, ~, ~, c_argin]=regexp(s_argin,',');
-[~, ~, ~, ~, ~, ~, c_argout]=regexp(s_argout,',');
+if ~isempty(ieq)
+    [~,iop,icp]=ffindmatching(s,ieq(1),'(',')',1);
+    % output arguments
+    isf=ieq+1;
+    s_argout=strtrim(s((iob+1):(icb-1)));
+    s_argout=strrep(s_argout,', ',',');
+    s_argout=strrep(s_argout,' ,',',');
+    s_argout=fremovespaces(s_argout);
+    s_argout=strrep(s_argout,' ',',');
+    % [~, ~, ~, ~, ~, ~, c_argin]=regexp(s_argin,',');
+    [~, ~, ~, ~, ~, ~, c_argout]=regexp(s_argout,',');
 
-% function name
-fname=strtrim(s(isf:(iop-1)));
-% arguments
-s_argin=s((iop+1):(icp-1));
+    % function name
+    fname=strtrim(s(isf:(iop-1)));
+    % arguments
+    s_argin=s((iop+1):(icp-1));
 
-% concatenation, don't care about inout for now...
-if ~isempty(s_argout) && ~isempty(s_argin)
-    s_args = [s_argin ' , ' s_argout];
-elseif isempty(s_argout) && ~isempty(s_argin)
-    s_args= s_argin;
-elseif isempty(s_argout) && isempty(s_argin)
-    s_args= s_argout;
-else
-    s_args='';
+    % concatenation, don't care about inout for now...
+    if ~isempty(s_argout) && ~isempty(s_argin)
+        s_args = [s_argin ' , ' s_argout];
+    elseif isempty(s_argout) && ~isempty(s_argin)
+        s_args= s_argin;
+    elseif isempty(s_argout) && isempty(s_argin)
+        s_args= s_argout;
+    else
+        s_args='';
+    end
+
+    scomm=s((icp+1):end);
+
+    % To tring
+    sf=['call ' fname '( ' s_args  ') ' scomm '\n'];
+
+    % Declarations
+    for iout = 1:length(c_argout)
+        v.name=strtrim(c_argout{iout});
+        [v.type, v.shape, v.prop] =fgetVarTypeFromName(v.name);
+        v.comment='';
+        decl_stack=fstack_push(decl_stack,v);
+    end
+
 end
-
-scomm=s((icp+1):end);
-
-% To tring
-sf=['call ' fname '( ' s_args  ') ' scomm '\n'];
-
-% Declarations
-for iout = 1:length(c_argout)
-    v.name=strtrim(c_argout{iout});
-    [v.type, v.shape, v.prop] =fgetVarTypeFromName(v.name);
-    v.comment='';
-    decl_stack=fstack_push(decl_stack,v);
-end
-
 
 end %function
 
